cee88558eb553b73538817cb6819afd94cbe9222
Loic Tiemani <loictiems@github.com>
2020-12-19 16:33:47 -0500

    Pushing updated Code SQL

--- netflix_query.sql
+++ netflix_query.sql
@@ -8,3 +8,9 @@ SELECT * FROM listed_in
 
 SELECT * FROM players
 SELECT COUNT(*) FROM players
+SELECT COUNT(*) FROM pg_rating
+
+--DROP TABLE country
+
+SELECT * FROM country
+SELECT COUNT(*) FROM country
