344953046af70dad977d8b4e4d7d2fcb9ef1ef22
Loic Tiemani <loictiems@github.com>
2020-12-19 11:48:45 -0500

    Pushing New tables SQL

--- netflix_query.sql
+++ netflix_query.sql
@@ -1,2 +1,10 @@
 SELECT count(*) FROM title
-DELETE FROM title
+
+SELECT COUNT(*) FROM director
+
+SELECT COUNT(*) FROM listed_in
+SELECT * FROM listed_in
+--DELETE FROM director
+
+SELECT * FROM players
+SELECT COUNT(*) FROM players
